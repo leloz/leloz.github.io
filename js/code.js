c3fad51a1fa49e7fb5f25e1ccc3a28e8681067ed
Leloz Suliman <leloz997@gmail.com>
2017-03-27 16:07:19 +0300

    add date&repos under the button

--- js/code.js
+++ js/code.js
@@ -3,7 +3,7 @@ var url ="https://api.github.com/users/leloz/repos?sort=created&per_page=5"
 $(document).ready(function(){
 
   clickButton();
- 
+  show(); 
 });
 
 function clickButton(){
@@ -14,5 +14,43 @@ function clickButton(){
    })
   })
 }
+
+function show(){
+
+  $.get(url, function(response) {
+    $.each(response,function(i,repo){
+      var d=new Date(repo.created_at)
+      $('#result').append('Created at: '+formatDate(d)+'<a href="'+repo.html_url+'" target="_blank">' + repo.name + '</a><br>'); 
+  $("date").append(response +'&nbsp;')
+    })
+  });
+};
+
+function formatDate(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
+}
+
+
+
+
+// <script type="text/javascript>
+  
+// </script>
+
+
+
+
+
+
+
+
+
 
 
