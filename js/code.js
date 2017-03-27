a795c40d8239d906304027c200633f0c60b7fa35
Leloz Suliman <leloz997@gmail.com>
2017-03-27 13:22:30 +0300

    add js folder

--- js/code.js
+++ js/code.js
@@ -1,1 +1,14 @@
-$.get 
+
+$(document).ready(function() {
+      // 1. show the name of a pokemon
+      // 2. show the image of a pokemon
+      $.get("https://github.com/leloz?tab=repositories", function(response) {
+         console.log(response)
+
+      })
+  }
+        // $('body').html('<h3>' + response.name + '</h3>'); 
+        // $('body').append('<img src=' + response.sprites.front_default + '>')
+  //   });
+  // });
+
